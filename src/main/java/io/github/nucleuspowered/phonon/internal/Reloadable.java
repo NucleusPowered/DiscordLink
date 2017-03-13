57f42c664fa79ac6c53bc1b1811873a33051155f
Daniel Naylor <git@drnaylor.co.uk>
2017-03-13 23:02:40 +0000

    Improve doc tags

--- src/main/java/io/github/nucleuspowered/phonon/internal/Reloadable.java
+++ src/main/java/io/github/nucleuspowered/phonon/internal/Reloadable.java
@@ -1,12 +1,12 @@
 package io.github.nucleuspowered.phonon.internal;
 
 /**
- * Marks a command or listener are reloadable.
+ * Marks a command or listener as reloadable - the {@link #onReload()} method will be called whenever the plugin config is reloaded.
  */
 public interface Reloadable {
 
     /**
-     * Fired when Phonon is reloaded.
+     * Fired when the Phonon plugin configuration is reloaded.
      */
     void onReload();
 }
