6eb1bbefe7c520dad5ce3946dd27717b5c4c163c
RysingDragon <jamalg960@gmail.com>
2017-05-31 20:40:02 -0500

    create discord bot

--- src/main/java/io/github/nucleuspowered/phonon/modules/core/CoreModule.java
+++ src/main/java/io/github/nucleuspowered/phonon/modules/core/CoreModule.java
@@ -1,8 +1,14 @@
 package io.github.nucleuspowered.phonon.modules.core;
 
+import io.github.nucleuspowered.phonon.discord.DiscordBot;
+import io.github.nucleuspowered.phonon.modules.core.config.CoreConfig;
 import io.github.nucleuspowered.phonon.modules.core.config.CoreConfigAdapter;
 import io.github.nucleuspowered.phonon.qsml.modulespec.ConfigurableModule;
+import net.dv8tion.jda.core.entities.Game;
+import net.dv8tion.jda.core.entities.Icon;
 import uk.co.drnaylor.quickstart.annotations.ModuleData;
+
+import java.util.Optional;
 
 @ModuleData(id = CoreModule.ID, name = "Core", isRequired = true)
 public class CoreModule extends ConfigurableModule<CoreConfigAdapter> {
@@ -15,5 +21,12 @@ public class CoreModule extends ConfigurableModule<CoreConfigAdapter> {
 
     @Override public void phononEnable() {
         super.phononEnable();
+
+        CoreConfig config = this.getAdapter().getNodeOrDefault();
+        String token = config.getToken();
+        DiscordBot bot = new DiscordBot(token);
+        if (!config.getGame().isEmpty()) {
+            bot.getJda().getPresence().setGame(Game.of(config.getGame()));
+        }
     }
 }
